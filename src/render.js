6d3e5f48f7c941ee4549b5bdd10e4f72107d2945
dassshha <dzhguleva@mail.ru>
2022-03-15 19:31:15 +0300

    add render funcs for components

--- src/render.js
+++ src/render.js
@@ -1,6 +1,6 @@
 const RENEDER_POSITION = {
   BEFOREBEGIN: 'beforebegin',
-  AFTERBEFIN: 'afterbegin',
+  AFTERBEGIN: 'afterbegin',
   BEFOREEND: 'beforeend',
   AFTEREND: 'afterend',
 };
@@ -9,4 +9,28 @@ const renderTemplate = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
 };
 
-export {RENEDER_POSITION, renderTemplate};
+const render = (container, element, place) => {
+  switch (place) {
+    case RENEDER_POSITION.BEFOREBEGIN:
+      container.before(element);
+      break;
+    case RENEDER_POSITION.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RENEDER_POSITION.BEFOREEND:
+      container.append(element);
+      break;
+    case RENEDER_POSITION.AFTEREND:
+      container.after(element);
+      break;
+  }
+};
+
+const createElement = (template) => {
+  const newElement = document.createElement('div');
+  newElement.innerHTML = template;
+
+  return newElement.firstChild;
+};
+
+export {RENEDER_POSITION, render, renderTemplate, createElement};
