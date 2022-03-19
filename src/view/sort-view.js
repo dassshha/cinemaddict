e3e293f973d47215e9143781f6b650ff3e698515
dassshha <dzhguleva@mail.ru>
2022-03-19 16:56:35 +0300

    fix sort template

--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -1,10 +1,11 @@
 import AbstractView from "./abstract-view.js";
+import {SORT_TYPE} from '../constants';
 
 const createSortTemplate = () => (
   `<ul class="sort">
-    <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-    <li><a href="#" class="sort__button">Sort by date</a></li>
-    <li><a href="#" class="sort__button">Sort by rating</a></li>
+    <li><a href="#" class="sort__button" data-sort-type="${SORT_TYPE.DEFAULT}">Sort by default</a></li>
+    <li><a href="#" class="sort__button" data-sort-type="${SORT_TYPE.DATE}">Sort by date</a></li>
+    <li><a href="#" class="sort__button" data-sort-type="${SORT_TYPE.RATING}">Sort by rating</a></li>
   </ul>`
 );
 
