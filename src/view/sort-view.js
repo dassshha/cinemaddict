7eeea80f5c01ec22f8e4c3d20f5a7274f700aa50
dassshha <dzhguleva@mail.ru>
2022-03-19 22:52:04 +0300

    add active class to default sort

--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -3,7 +3,7 @@ import {SORT_TYPE} from '../constants';
 
 const createSortTemplate = () => (
   `<ul class="sort">
-    <li><a href="#" class="sort__button" data-sort-type="${SORT_TYPE.DEFAULT}">Sort by default</a></li>
+    <li><a href="#" class="sort__button sort__button--active" data-sort-type="${SORT_TYPE.DEFAULT}">Sort by default</a></li>
     <li><a href="#" class="sort__button" data-sort-type="${SORT_TYPE.DATE}">Sort by date</a></li>
     <li><a href="#" class="sort__button" data-sort-type="${SORT_TYPE.RATING}">Sort by rating</a></li>
   </ul>`
