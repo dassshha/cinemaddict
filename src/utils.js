2648b8ecce335c94dd7b0227a1eae6df32f63fc7
dassshha <dzhguleva@mail.ru>
2022-03-28 22:54:07 +0300

    move updateItem to model

--- src/utils.js
+++ src/utils.js
@@ -10,17 +10,5 @@ const sortFilmsByRatingDown = (film1, film2) => film2.rating - film1.rating;
 const sortFilmsByDateDown = (film1, film2) => film1.dateRelease.isBefore(film2.dateRelease) ? 1 : -1;
 
 const isEscapeKey = (evt) => evt.key === 'Escape' || evt.key === 'Esc';
-const updateItem = (items, update) => {
-  const index = items.findIndex((item) => item.id === update.id);
 
-  if (index === -1) {
-    return items;
-  }
-
-  return [
-    ...items.slice(0, index),
-    update,
-    ...items.slice(index + 1),
-  ];
-};
-export {getRandomNumber, isEscapeKey, updateItem, sortFilmsByRatingDown, sortFilmsByDateDown};
+export {getRandomNumber, isEscapeKey, sortFilmsByRatingDown, sortFilmsByDateDown};
