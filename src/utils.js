6e73593f6d964ef6b5839c0496d61876a4ffd7db
dassshha <dzhguleva@mail.ru>
2022-03-29 20:53:55 +0300

    add filter func

--- src/utils.js
+++ src/utils.js
@@ -13,10 +13,23 @@ const sortFilmsByDateDown = (film1, film2) => film1.dateRelease.isBefore(film2.d
 
 const isEscapeKey = (evt) => evt.key === 'Escape' || evt.key === 'Esc';
 
-const filter = {
-  [FILTER_TYPE.WATCHLIST]: (films) => films.filter((film) => film.addToWatchlist),
-  [FILTER_TYPE.HISTORY]: (films) => films.filter((film) => film.alreadyWatched),
-  [FILTER_TYPE.FAVORITES]: (films) => films.filter((film) => film.isFavorite)
+// const filter = {
+//   [FILTER_TYPE.WATCHLIST]: (films) => films.filter((film) => film.addToWatchlist),
+//   [FILTER_TYPE.HISTORY]: (films) => films.filter((film) => film.alreadyWatched),
+//   [FILTER_TYPE.FAVORITES]: (films) => films.filter((film) => film.isFavorite)
+// };
+
+const filter = (films, filterType) => {
+  switch (filterType) {
+    case FILTER_TYPE.WATCHLIST:
+      return films.filter((film) => film.addToWatchlist);
+    case FILTER_TYPE.HISTORY:
+      return films.filter((film) => film.alreadyWatched);
+    case FILTER_TYPE.FAVORITES:
+      return films.filter((film) => film.isFavorite);
+  }
+  return films;
 };
+
 
 export {getRandomNumber, isEscapeKey, sortFilmsByRatingDown, sortFilmsByDateDown, filter};
